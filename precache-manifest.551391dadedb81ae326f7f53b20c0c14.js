a101d5c80c3c7694f9bc811e2d0639aeac824a05
michaelOch <ochuenwikechukwudi@hotmail.com>
2020-11-02 01:31:22 -0800

    Updates

---
+++ precache-manifest.551391dadedb81ae326f7f53b20c0c14.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f9aaa8916ae33d4f1f3c5919ca864bca",
+    "url": "/yfdot-app/index.html"
+  },
+  {
+    "revision": "a987b4e01b783f0ed9e5",
+    "url": "/yfdot-app/static/css/2.25d2a862.chunk.css"
+  },
+  {
+    "revision": "4a079b830c1ad24d9c22",
+    "url": "/yfdot-app/static/css/main.1b19be70.chunk.css"
+  },
+  {
+    "revision": "a987b4e01b783f0ed9e5",
+    "url": "/yfdot-app/static/js/2.4cd73e74.chunk.js"
+  },
+  {
+    "revision": "4df4c58fd3db8af31000da1d75d059ea",
+    "url": "/yfdot-app/static/js/2.4cd73e74.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4a079b830c1ad24d9c22",
+    "url": "/yfdot-app/static/js/main.5a896daa.chunk.js"
+  },
+  {
+    "revision": "75a3b020b488eeeca2d5",
+    "url": "/yfdot-app/static/js/runtime-main.8008463a.js"
+  },
+  {
+    "revision": "2602a4d606616c0f3b34f6e1eff8aac7",
+    "url": "/yfdot-app/static/media/cryptomaster.2602a4d6.png"
+  },
+  {
+    "revision": "f10c467f57e99621b280ebb10c60bc8b",
+    "url": "/yfdot-app/static/media/digifinex.f10c467f.png"
+  },
+  {
+    "revision": "be5c07a5510afd2d0ae704829450c7ac",
+    "url": "/yfdot-app/static/media/tokeninfo.be5c07a5.webp"
+  },
+  {
+    "revision": "b9a39a2d9ca304d5e3f190fadca0e1af",
+    "url": "/yfdot-app/static/media/uniswap.b9a39a2d.png"
+  },
+  {
+    "revision": "e0655e324d829f4974a99db6a0a6527a",
+    "url": "/yfdot-app/static/media/whitepaper.e0655e32.png"
+  }
+]);
